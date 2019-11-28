11827eca3d881a27725285e6795adecb44ecdb85
yanfei00 <enjoy_yanfei@163.com>
2019-11-28 16:23:15 +0800

    初始化项目并配置

---
+++ src/components/common/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="tabbar-item" @click="itemClick">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else ><slot name="item-icon-active"></slot></div>
+      <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props:{
+      path:{
+        type:String,
+      },
+      activeColor: {
+        type:String,
+        default:'red'
+      }
+    },
+    data(){
+      return {
+      }
+    },
+    computed:{
+      isActive() {
+        return this.$route.path.indexOf(this.path) != -1
+      },
+      activeStyle() {
+        return this.isActive ? {color:this.activeColor} : {}
+      }
+    },
+    methods:{
+      itemClick() {
+        this.$router.push(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .tabbar-item {
+    flex: 1;
+    font-size: 12px;
+    height: 59px;
+    text-align: center;
+  }
+  .tabbar-item img {
+    width: 24px;
+    height: 24px;
+    margin: 8px 0 6px 0;
+    vertical-align: middle;
+  }
+
+  /*.active {*/
+    /*color: red;*/
+  /*}*/
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// 配置项目路径别名
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views'
+      }
+    }
+  }
+}
